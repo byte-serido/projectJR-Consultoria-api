f46c800e2689ffc36e7489ef0b74c123e8640680
JFmaia <jfmaia741@gmail.com>
2023-01-10 19:01:20 -0300

    feat: Criando modulo que criar user..

---
+++ src/modules/createUser.ts
@@ -0,0 +1,33 @@
+import { PrismaClient, User } from "@prisma/client"
+export const prisma = new PrismaClient;
+
+export interface typeUser{
+    username: string,
+    email: string, 
+    password: string, 
+    name: string,
+    mod: string
+}
+
+export class CreateUser{
+    async execute({name, email, username, password, mod}:typeUser): Promise<User>{
+
+     //Criptografando a senha
+     const bcrypt = require('bcrypt');
+     const hash = await bcrypt.hash(password, 10);
+     password = hash;
+     
+     //Criar usuário
+     const newUser = await prisma.user.create({
+         data: {
+            username: username,
+            name: name,
+            email: email,
+            password: password,
+            mod: mod,
+         }
+     });
+ 
+     return newUser;
+    }
+ }
