b168e4f779f3a214f90bac9f53c68edf123482d8
JFmaia <jfmaia741@gmail.com>
2023-01-10 19:02:13 -0300

    feat: Modificando o server.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
+import bodyParser from "body-parser";
 import express from "express";
-
-const api = express();
-api.use(express.json());
+import { routes } from "./routes";
 
 
-api.listen(3333, () => console.log("Server is running in port 3333!"));
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(routes);
+
+app.listen(3333, () => console.log("Server is running in port 3333!"));
