fed9dfcc340bb25707c9b4e5f6f8dd4c5d1aecc6
JFmaia <jfmaia741@gmail.com>
2023-01-15 09:20:18 -0300

    feat: Adicionando o middle na rota de projects

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { authRoutes } from "./auth.routes";
-
+import { projectRoutes } from "./project.routes";
 const routes = Router();
 
 routes.use("/auth",authRoutes);
+routes.use("/projects",projectRoutes);
 
 export {routes};
