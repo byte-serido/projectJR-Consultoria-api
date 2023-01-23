716caeeb48e2b8f18ce480ab674a95b19292a3b3
Tallys-aureliano <tallysaureliano@outlook.com>
2023-01-23 20:49:50 -0300

    feat: solution create controller

---
+++ src/app/controllers/createSolutionController.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+import { CreateSolution } from "../usercases/createSolution";
+
+export const prisma = new PrismaClient;
+
+export class CreateSolutionController {
+    async handle(req: Request, res: Response) {
+
+        const { name, description, imgUrl } = req.body;
+        
+        try {
+            if (await prisma.solution.findUnique({ where: { name: name } })) {
+                return res.status(400).send({ error: "Solution already exists" })
+            }
+
+            const create = new CreateSolution();
+            const solution = await create.execute({ name, description, imgUrl });
+
+            return res.send({
+                solution
+            },);
+
+        }   catch(err){
+            return res.status(400).send(err);
+        }  
+    };
+}
