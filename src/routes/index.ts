1ea7e79c54c065f271acb4998f3a9d61c94ac76d
Tallys-aureliano <tallysaureliano@outlook.com>
2023-01-23 20:53:37 -0300

    feat: adding solution routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { authRoutes } from "./auth.routes";
 import { projectRoutes } from "./project.routes";
+import { solutionRoutes } from "./solution.routes";
 const routes = Router();
 
 routes.use("/auth",authRoutes);
 routes.use("/projects",projectRoutes);
+routes.use("/solutions",solutionRoutes);
 
 export {routes};
