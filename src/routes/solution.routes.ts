0cf3b9b98799493f40f4021f7d55d7c1669c2f59
Tallys-aureliano <tallysaureliano@outlook.com>
2023-01-24 17:02:44 -0300

    feat: aded route of getAll from solutions

--- src/routes/solution.routes.ts
+++ src/routes/solution.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { CreateSolutionController } from "../app/controllers/createSolutionController";
+import { GetAllSolutionController } from "../app/controllers/getSolutionsController";
 
 const createSolutionController = new CreateSolutionController();
+const getAllSolutionController = new GetAllSolutionController();
 const solutionRoutes = Router();
 
 solutionRoutes.post("/create", createSolutionController.handle);
+solutionRoutes.get("/getAll", getAllSolutionController.handle);
 
 export {solutionRoutes};
