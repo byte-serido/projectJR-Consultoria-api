a53f9f602a636dee291867564221ed1309b7741f
Tallys-aureliano <tallysaureliano@outlook.com>
2023-01-25 13:43:20 -0300

    feat: update controller

---
+++ src/app/controllers/updateSolutionController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import { UpdateSolution } from "../usercases/updateSolution";
+
+export const prisma = new PrismaClient();
+
+export class UpdateSolutionController {
+    async handle(req: Request, res:Response) {
+
+        const {id,name, description, imgUrl} = req.body;
+
+        try {
+            if (await prisma.solution.findMany({
+                where: {
+                    id: id
+                }
+            })) {
+                const update = new UpdateSolution();
+                const solution = await update.execute({
+                    id, name, description, imgUrl
+                });
+
+                return res.send({solution})
+            }
+
+            return res.status(400).send({ error: "Solution not exists" })
+        } catch(err){
+            return res.status(400).send(err);
+        }
+    };
+}
