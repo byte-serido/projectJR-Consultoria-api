0ac29ea0bf46baad4efbbfde6124618a99c3158f
Tallys-aureliano <tallysaureliano@outlook.com>
2023-01-25 13:46:55 -0300

    feat: added solution update routes

--- src/routes/solution.routes.ts
+++ src/routes/solution.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { CreateSolutionController } from "../app/controllers/createSolutionController";
 import { GetAllSolutionController } from "../app/controllers/getSolutionsController";
+import { UpdateSolutionController } from "../app/controllers/updateSolutionController";
 
 const createSolutionController = new CreateSolutionController();
 const getAllSolutionController = new GetAllSolutionController();
+const updateSolutionController = new UpdateSolutionController();
 const solutionRoutes = Router();
 
 solutionRoutes.post("/create", createSolutionController.handle);
 solutionRoutes.get("/getAll", getAllSolutionController.handle);
+solutionRoutes.put("/update", updateSolutionController.handle);
 
 export {solutionRoutes};
