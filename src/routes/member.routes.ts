7631af43dc87737a24de8cdb38a0b4eddefacd80
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-01-25 13:27:42 -0300

    chore: add get members route

--- src/routes/member.routes.ts
+++ src/routes/member.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { CreateMemberController } from "../app/controllers/createMemberController";
+import { GetMembersController } from "../app/controllers/getMemberController";
 
 const createMemberController = new CreateMemberController();
+const getMembersController = new GetMembersController();
 const memberRoutes = Router();
 
 memberRoutes.post("/create",createMemberController.handle)
+memberRoutes.get("/get",getMembersController.handle)
 
 export {memberRoutes};
