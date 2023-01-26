9c0a0b73abce7adbe3ff7f6591614bdda4e8751a
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-01-26 13:59:04 -0300

    chore: add rout for delet member

--- src/routes/member.routes.ts
+++ src/routes/member.routes.ts
@@ -2,14 +2,17 @@ import { Router } from "express";
 import { CreateMemberController } from "../app/controllers/createMemberController";
 import { GetMembersController } from "../app/controllers/getMemberController";
 import { UpdateMemberController } from "../app/controllers/updateMemberController";
+import { DeletMemberController } from "../app/controllers/deleteMemberController";
 
 const createMemberController = new CreateMemberController();
 const getMembersController = new GetMembersController();
 const updateMemberController = new UpdateMemberController();
+const deletMemberController = new DeletMemberController();
 const memberRoutes = Router();
 
-memberRoutes.post("/create",createMemberController.handle)
-memberRoutes.get("/get",getMembersController.handle)
+memberRoutes.post("/create",createMemberController.handle);
+memberRoutes.get("/get",getMembersController.handle);
 memberRoutes.put("/update",updateMemberController.handle);
+memberRoutes.delete("/delet",deletMemberController.handle);
 
 export {memberRoutes};
