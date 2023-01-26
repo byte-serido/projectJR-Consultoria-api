e6682c926bc1279cd7200710b90c55261fc48cb3
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-01-26 14:52:21 -0300

    mod routes/index.ts #3

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { authRoutes } from "./auth.routes";
+import { contactRoutes } from "./contact.routes";
 import { projectRoutes } from "./project.routes";
 const routes = Router();
 
-routes.use("/auth",authRoutes);
-routes.use("/projects",projectRoutes);
+routes.use("/auth", authRoutes);
+routes.use("/projects", projectRoutes);
+routes.use("/contacts", contactRoutes);
 
-export {routes};
+export { routes };
