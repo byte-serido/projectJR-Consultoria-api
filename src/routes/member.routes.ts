e043731df51e63b8fb4a3fb7af8b9a6ebb6a14a0
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-01-26 13:32:48 -0300

    chore: add route for member update

--- src/routes/member.routes.ts
+++ src/routes/member.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { CreateMemberController } from "../app/controllers/createMemberController";
 import { GetMembersController } from "../app/controllers/getMemberController";
+import { UpdateMemberController } from "../app/controllers/updateMemberController";
 
 const createMemberController = new CreateMemberController();
 const getMembersController = new GetMembersController();
+const updateMemberController = new UpdateMemberController();
 const memberRoutes = Router();
 
 memberRoutes.post("/create",createMemberController.handle)
 memberRoutes.get("/get",getMembersController.handle)
+memberRoutes.put("/update",updateMemberController.handle);
 
 export {memberRoutes};
