22da259faafde5bfad658df9d807c615d0cc8b8d
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-01-27 14:23:34 -0300

    mod routes/contact.routes.ts #3

--- src/routes/contact.routes.ts
+++ src/routes/contact.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { CreateContactController } from "../app/controllers/contactControllers/createContactController";
+import { GetAllContactsController } from "../app/controllers/contactControllers/getAllContactsController";
 
 const createContactController = new CreateContactController();
+const getAllContactsController = new GetAllContactsController();
 const contactRoutes = Router();
 
 contactRoutes.post("/create", createContactController.handle);
+contactRoutes.get("/", getAllContactsController.handle);
 
 export { contactRoutes };
