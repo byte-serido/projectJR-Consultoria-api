e8e0c2294627d780c280b02fcdc1bf1c8eb96977
Tallys-aureliano <tallysaureliano@outlook.com>
2023-01-27 09:36:40 -0300

    chore: deleting routes from getOne

--- src/routes/solution.routes.ts
+++ src/routes/solution.routes.ts
@@ -3,18 +3,15 @@ import { CreateSolutionController } from "../app/controllers/solutionsController
 import { GetAllSolutionController } from "../app/controllers/solutionsController/getSolutionsController";
 import { UpdateSolutionController } from "../app/controllers/solutionsController/updateSolutionController";
 import { DeleteSolutionController } from "../app/controllers/solutionsController/deleteSolutionController";
-import { GetOneSolutionController } from "../app/controllers/solutionsController/getOneSolutionController";
 
 const createSolutionController = new CreateSolutionController();
 const getAllSolutionController = new GetAllSolutionController();
-const getOneSolutionController = new GetOneSolutionController();
 const updateSolutionController = new UpdateSolutionController();
 const deleteSolutionController = new DeleteSolutionController();
 const solutionRoutes = Router();
 
 solutionRoutes.post("/create", createSolutionController.handle);
 solutionRoutes.get("/getAll", getAllSolutionController.handle);
-solutionRoutes.get("/getOne", getOneSolutionController.handle);
 solutionRoutes.put("/update", updateSolutionController.handle);
 solutionRoutes.delete("/delete", deleteSolutionController.handle);
 
