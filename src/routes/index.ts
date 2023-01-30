f7e948cb7cf2b9068cca8a11fe9e19e018d67a28
JFmaia <jfmaia741@gmail.com>
2023-01-30 10:27:11 -0300

    feat: adicionando rota user. #9

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,9 +4,12 @@ import { contactRoutes } from "./contact.routes";
 import { projectRoutes } from "./project.routes";
 import { solutionRoutes } from "./solution.routes";
 import { memberRoutes } from "./member.routes";
+import { userRoutes } from "./user.routes";
+
 const routes = Router();
 
 routes.use("/auth", authRoutes);
+routes.use("/user", userRoutes);
 routes.use("/projects", projectRoutes);
 routes.use("/solutions", solutionRoutes);
 routes.use("/member", memberRoutes);
