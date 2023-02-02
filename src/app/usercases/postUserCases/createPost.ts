b24a0210df64252d99ac293e013068474de53b33
Tallys-aureliano <tallysaureliano@outlook.com>
2023-02-02 11:42:46 -0300

    feat: create use case #13

---
+++ src/app/usercases/postUserCases/createPost.ts
@@ -0,0 +1,34 @@
+import { Post, PrismaClient } from "@prisma/client";
+
+export const prisma = new PrismaClient();
+
+export interface typePost {
+
+    title: string;
+    description: string;
+    autor: string;
+    imgUrl: string;
+
+}
+
+export class CreatePost {
+    async execute({
+        title,
+        description,
+        autor,
+        imgUrl,
+    }: typePost): Promise<Post> {
+        const newPost = await prisma.post
+            .create({
+
+                data: {
+                    title: title,
+                    description: description,
+                    autor: autor,
+                    imgUrl: imgUrl,
+                },
+            });
+
+        return newPost;
+    }
+}
