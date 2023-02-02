9a58ef223d8a5d951aabf658c1a6951a99f88279
Tallys-aureliano <tallysaureliano@outlook.com>
2023-02-02 08:54:27 -0300

    chore: post route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,14 +5,16 @@ import { projectRoutes } from "./project.routes";
 import { solutionRoutes } from "./solution.routes";
 import { memberRoutes } from "./member.routes";
 import { userRoutes } from "./user.routes";
+import { postRoutes } from "./post.routes";
 
 const routes = Router();
 
 routes.use("/auth", authRoutes);
 routes.use("/user", userRoutes);
 routes.use("/projects", projectRoutes);
-routes.use("/solutions", solutionRoutes);
+routes.use("/solution", solutionRoutes);
 routes.use("/member", memberRoutes);
 routes.use("/contacts", contactRoutes);
+routes.use("/post", postRoutes);
 
 export { routes };
