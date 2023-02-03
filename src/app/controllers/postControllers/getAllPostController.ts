5cbf71e95fb4df4f4b5690d16df14d225cc589e0
Tallys-aureliano <tallysaureliano@outlook.com>
2023-02-02 22:09:59 -0300

    feat: getAll controller #13

---
+++ src/app/controllers/postControllers/getAllPostController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { GetAllPost } from "../../usercases/postUserCases/getAllPost";
+import { PrismaClient } from "@prisma/client";
+
+export const prisma = new PrismaClient();
+
+export class GetAllPostController {
+    async handle(req: Request, res: Response) {
+
+        try {
+
+            const getAllPost = new GetAllPost();
+            const result = await getAllPost.execute()
+
+            if (result.length == 0) {
+                return res.status(400).send({ error: "Nenhum post criado" })
+            }
+
+            return res.status(200).json(result);
+
+        } catch (err) {
+            return res.status(400).send({ error: "Erro na busca por posts." })
+        }
+    }
+}
