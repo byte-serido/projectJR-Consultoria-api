86a11a07d93927375232a07c9df5ce7340f95b50
Tallys-aureliano <tallysaureliano@outlook.com>
2023-02-02 22:10:43 -0300

    feat: route gelAll #13

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { CreatePostController } from "../app/controllers/postControllers/createPostController";
+import { GetAllPostController } from "../app/controllers/postControllers/getAllPostController";
 
 const postRoutes = Router();
 
 const createPost = new CreatePostController();
+const getAllPost = new GetAllPostController();
 
 postRoutes.post("/create", createPost.handle);
-
+postRoutes.get("/getAll", getAllPost.handle)
 
 export { postRoutes }
