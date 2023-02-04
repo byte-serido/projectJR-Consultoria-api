8944abc337682960b1cae47a5ba929264fb684ba
Tallys-aureliano <tallysaureliano@outlook.com>
2023-02-03 21:24:33 -0300

    feat: update route post #13

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { CreatePostController } from "../app/controllers/postControllers/createPostController";
 import { GetAllPostController } from "../app/controllers/postControllers/getAllPostController";
+import { UpdatePost } from "../app/controllers/postControllers/updatePostController";
 
 const postRoutes = Router();
 
 const createPost = new CreatePostController();
 const getAllPost = new GetAllPostController();
+const updatePost = new UpdatePost();
 
 postRoutes.post("/create", createPost.handle);
-postRoutes.get("/getAll", getAllPost.handle)
+postRoutes.get("/getAll", getAllPost.handle);
+postRoutes.put("/update", updatePost.handle);
 
 export { postRoutes }
