2ba543e6eb30d2b6b66c124da5dbc42b2067c286
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-02-09 14:58:59 -0300

    add updateCommentController #14

---
+++ src/app/controllers/commentControllers/updateCommentController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+import { UpdateComment } from "../../usercases/commentUserCases/updateComment";
+
+export const prisma = new PrismaClient();
+
+export class UpdateCommentController {
+  async handle(req: Request, res: Response) {
+    const { id, authorName, text, posts } = req.body;
+
+    try {
+      if (!(await prisma.comment.findUnique({ where: { id: id } }))) {
+        return res.status(400).send({ error: "Comment not foundt!" });
+      }
+      const update = new UpdateComment();
+      const comment = await update.execute({
+        id,
+        authorName,
+        text,
+        posts,
+      });
+
+      return res
+        .status(200)
+        .send({ comment, sucess: "Comment update successful!" });
+    } catch (error) {
+      return res.status(400).send({ error: "Comment update failed!" });
+    }
+  }
+}
