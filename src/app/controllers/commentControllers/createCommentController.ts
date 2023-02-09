fdb828d575537abe7e7181af2e6aa1ff2e2c4377
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-02-08 22:46:13 -0300

    add getAllComments, mod createCommentController.ts #14

--- src/app/controllers/commentControllers/createCommentController.ts
+++ src/app/controllers/commentControllers/createCommentController.ts
@@ -1,18 +1,17 @@
 import { Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Post } from "@prisma/client";
 import { CreateComment } from "../../usercases/commentUserCases/createComment";
 
 export const prisma = new PrismaClient();
 
 export class CreateCommentController {
   async handle(req: Request, res: Response) {
-    const { id, authorName, text, posts } = req.body;
+    const { authorName, text, posts } = req.body;
 
     try {
-      if (await prisma.comment.findUnique({ where: { id: id } })) {
-        return res.status(400).send({ erro: "Contact already exists !" });
+      if (!(await prisma.post.findUnique({ where: { id: posts } }))) {
+        return res.status(400).send({ erro: "Post does not exists!" });
       }
-
       const create = new CreateComment();
       const comment = await create.execute({
         authorName,
@@ -24,7 +23,7 @@ export class CreateCommentController {
         comment,
       });
     } catch (error) {
-      return res.status(400).send({ error: "Registration failed !" });
+      return res.status(400).send({ error: "Comment failed !" });
     }
   }
 }
