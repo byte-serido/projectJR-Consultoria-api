042e72bf08f5b422002f85f84c2059b4ddaecca4
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-02-09 15:01:27 -0300

    mod comment.routes #14

--- src/routes/comment.routes.ts
+++ src/routes/comment.routes.ts
@@ -1,13 +1,16 @@
 import { Router } from "express";
 import { CreateCommentController } from "../app/controllers/commentControllers/createCommentController";
 import { GetAllCommentsController } from "../app/controllers/commentControllers/getAllCommentsController";
+import { UpdateCommentController } from "../app/controllers/commentControllers/updateCommentController";
 
 const commentRoutes = Router();
 
 const createComment = new CreateCommentController();
 const getAllComments = new GetAllCommentsController();
+const updateComment = new UpdateCommentController();
 
 commentRoutes.post("/create", createComment.handle);
 commentRoutes.get("/getAll", getAllComments.handle);
+commentRoutes.put("/update", updateComment.handle);
 
 export { commentRoutes };
