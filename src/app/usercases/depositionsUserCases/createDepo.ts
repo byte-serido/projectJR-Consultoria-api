ff2a5cdee355a988f570b62490b9b878ed69e63e
Tallys-aureliano <tallysaureliano@outlook.com>
2023-02-11 19:29:04 -0300

    feat: create use case #19

---
+++ src/app/usercases/depositionsUserCases/createDepo.ts
@@ -0,0 +1,37 @@
+import { Deposition, PrismaClient } from "@prisma/client";
+
+
+export const prisma = new PrismaClient();
+
+export interface typeDepo {
+
+    name: string;
+    testimony: string;
+    office: string;
+    company: string;
+    imgUrl: string;
+}
+
+export class CreateDepo {
+    async execute({
+        name,
+        testimony,
+        office,
+        company,
+        imgUrl,
+    }: typeDepo): Promise<Deposition> {
+
+        const newDepo = await prisma.deposition.create({
+
+            data: {
+                name: name,
+                testimony: testimony,
+                office: office,
+                company: company,
+                imgUrl: imgUrl,
+            },
+        });
+
+        return newDepo;
+    }
+}
