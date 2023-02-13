6b7385a978ae1b407a8160e36367a62729c5573a
Tallys-aureliano <tallysaureliano@outlook.com>
2023-02-13 16:06:25 -0300

    feat: update depo.routes #19

--- src/routes/depo.routes.ts
+++ src/routes/depo.routes.ts
@@ -1,14 +1,17 @@
 import { Router } from "express";
 import { CreateDepoController } from "../app/controllers/depositionsController/createDepoController";
 import { GelAllDepoController } from "../app/controllers/depositionsController/getAllDepoController";
+import { UpdateDepos } from "../app/controllers/depositionsController/updateDepoController";
 
 const depoRoutes = Router();
 
 const createDepo = new CreateDepoController();
 const getAllDepo = new GelAllDepoController()
+const upDepo = new UpdateDepos();
 
 depoRoutes.post("/create", createDepo.handle);
 depoRoutes.get("/getAll", getAllDepo.handle);
+depoRoutes.put("/update", upDepo.handle);
 
 
 export {depoRoutes}
