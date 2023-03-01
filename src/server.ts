fad25df1c64c93de9ab5ed45e642a2c9503c4fd0
JFmaia <jfmaia741@gmail.com>
2023-03-01 11:56:49 -0300

    feat: Adicionando o cors.

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,11 @@ import bodyParser from "body-parser";
 import express from "express";
 import { routes } from "./routes";
 const history = require('connect-history-api-fallback');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(routes);
