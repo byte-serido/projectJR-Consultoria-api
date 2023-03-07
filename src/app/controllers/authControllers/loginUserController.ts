f133e375759cdb1e00f7b64cdb74436fd56803be
JFmaia <jfmaia741@gmail.com>
2023-03-07 07:50:11 -0300

    feat: Troca de package.
    bcrypt por bcryptjs.

--- src/app/controllers/authControllers/loginUserController.ts
+++ src/app/controllers/authControllers/loginUserController.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import { PrismaClient} from "@prisma/client"
 import { generateToken } from "../../../config/generateToken";
 export const prisma = new PrismaClient;
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 export class LoginUserController{
     async handle(req: Request, res: Response){
