da136dafce312236ba9b0a64c53de84b28799279
JFmaia <jfmaia741@gmail.com>
2023-03-26 12:16:30 -0300

    feat: Revertendo mudança.

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({
-    origin:"http://localhost:8080"
-}));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(routes);
