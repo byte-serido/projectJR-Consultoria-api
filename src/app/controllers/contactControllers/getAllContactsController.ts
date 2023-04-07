d8a95d927ab609b67effba279d9662c2e24db713
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-04-07 16:01:57 -0300

    :books: docs: doc getAllContactsController (#36)

--- src/app/controllers/contactControllers/getAllContactsController.ts
+++ src/app/controllers/contactControllers/getAllContactsController.ts
@@ -1,10 +1,23 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import { GetAllContacts } from "../../usercases/contactUserCases/getAllContacts";
-import {StatusCodes} from "http-status-codes"
+import { StatusCodes } from "http-status-codes";
 
 export const prisma = new PrismaClient();
 
+/**
+ *  Essa é a classe controller do GetAllContacts
+ *
+ *  Antes de listar os contatos, ela testa se existe algum contato,
+ *  se não existir retorna status:
+ *  PAYMENT_REQUIRED = 402
+ *
+ *  Se correu tudo bem retorna o status:
+ *  OK = 200
+ *
+ *  Se ocorreu algum erro no processo retorna status 400.
+ *
+ */
 export class GetAllContactsController {
   async handle(req: Request, res: Response) {
     try {
