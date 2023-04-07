f2d5b9c0dbc1b8290842cdfcc290b05fbc595c6a
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-04-07 18:25:35 -0300

    :books: docs: doc deletePostController (#36)

--- src/app/controllers/postControllers/deletePostController.ts
+++ src/app/controllers/postControllers/deletePostController.ts
@@ -4,31 +4,42 @@ import { StatusCodes } from "http-status-codes";
 
 export const prisma = new PrismaClient();
 
+/**
+ *  Essa é a classe controller do DeletePost
+ *
+ *  Ela busca e deleta uma postagem.
+ *
+ *  Se correu tudo bem retorna o depoimento e status:
+ *  CREATED = 201
+ *
+ *  Se ocorreu algum erro no processo retorna status:
+ *  BAD_REQUEST = 400
+ *
+ */
 export class DeletePost {
-    async handle(req: Request, res: Response) {
+  async handle(req: Request, res: Response) {
+    try {
+      const { id } = req.body;
 
-        try {
+      await prisma.comment.deleteMany({
+        where: {
+          posts: id,
+        },
+      });
 
-            const { id } = req.body;
+      await prisma.post.delete({
+        where: {
+          id: id,
+        },
+      });
 
-            await prisma.comment.deleteMany({
-                where: {
-                    posts: id,
-                },
-            },);
-
-            await prisma.post.delete({
-                where: {
-                    id: id
-                }
-            },);
-
-            return res.status(StatusCodes.CREATED).send({
-                sucess: "Post foi apagado com sucesso!"
-            });
-
-        } catch (error) {
-            return res.status(StatusCodes.BAD_REQUEST).send({ error: "Não foi possível apagar esse post." });
-        }
+      return res.status(StatusCodes.CREATED).send({
+        sucess: "Post foi apagado com sucesso!",
+      });
+    } catch (error) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .send({ error: "Não foi possível apagar esse post." });
     }
+  }
 }
