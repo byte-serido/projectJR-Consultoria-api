a605c510b5afed17ab7b98cc8b8f6b3dd248ce68
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-04-07 15:59:38 -0300

    :books: docs: doc deleteContactController (#36)

--- src/app/controllers/contactControllers/deleteContactController.ts
+++ src/app/controllers/contactControllers/deleteContactController.ts
@@ -1,10 +1,24 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import { DeleteContact } from "../../usercases/contactUserCases/deleteContact";
-import {StatusCodes} from "http-status-codes"
+import { StatusCodes } from "http-status-codes";
 
 export const prisma = new PrismaClient();
 
+/**
+ *  Essa é a classe controller do DeleteContact
+ *
+ *  Antes de deletar um contato, ela testa se o contato existe,
+ *  se não existir retorna status:
+ *  BAD_REQUEST = 400
+ *
+ *  Se correu tudo bem retorna o status:
+ *  CREATED = 201
+ *
+ *  Se ocorreu algum erro no processo retorna status:
+ *  NOT_FOUND = 404
+ *
+ */
 export class DeleteContactController {
   async handle(req: Request, res: Response) {
     const { id } = req.body;
