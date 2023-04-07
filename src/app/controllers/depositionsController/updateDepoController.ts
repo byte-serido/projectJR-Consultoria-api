3bcccf5c3f6a46825eb34216c471c6b40ce72e37
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-04-07 17:14:44 -0300

    :books: docs: doc updateDepoController (#36)

--- src/app/controllers/depositionsController/updateDepoController.ts
+++ src/app/controllers/depositionsController/updateDepoController.ts
@@ -5,33 +5,59 @@ import { StatusCodes } from "http-status-codes";
 
 export const prisma = new PrismaClient();
 
+/**
+ *  Essa é a classe controller do UpdateDepo
+ *
+ *  Antes de editar um depoimento, ela testa se o depoimento existe,
+ *  e se tem alguma informação vazia, caso exista informação vazia
+ *  retorna status:
+ *  UNAUTHORIZED = 401
+ *
+ *  Se correu tudo bem, retorna o depoimento e status:
+ *  CREATED = 201
+ *
+ *  Se ocorreu algum erro no processo retorna status:
+ *  NOT_FOUND = 404
+ */
 export class UpdateDepos {
-    async handle(req: Request, res: Response) {
-        const { id, name, testimony, office, company, imgUrl } = req.body;
+  async handle(req: Request, res: Response) {
+    const { id, name, testimony, office, company, imgUrl } = req.body;
 
-        try {
+    try {
+      if (
+        await prisma.deposition.findMany({
+          where: {
+            id: id,
+          },
+        })
+      ) {
+        const updateDepo = new UpdateDepo();
+        await updateDepo.execute({
+          id,
+          name,
+          testimony,
+          office,
+          company,
+          imgUrl,
+        });
 
-            if (await prisma.deposition.findMany({
-                where: {
-                    id: id,
-                }
-            })) {
-                const updateDepo = new UpdateDepo();
-                await updateDepo.execute({
-                    id, name, testimony, office, company, imgUrl
-                });
-
-                if (!name || !testimony || !company ) {
-                    return res.status(StatusCodes.UNAUTHORIZED).send({ Error: "O Depoimento deve conter: Autor, Descrição e Empresa como obrigatório." })
-                }
-
-                return res.status(StatusCodes.CREATED).send({
-                    Sucess: "Depoiemnto Atualizado, obrigado!"
-                })
-            }
-
-        } catch (error) {
-            return res.status(StatusCodes.NOT_FOUND).send({ error: "Erro ao cadastrar depoimento." });
+        if (!name || !testimony || !company) {
+          return res
+            .status(StatusCodes.UNAUTHORIZED)
+            .send({
+              Error:
+                "O Depoimento deve conter: Autor, Descrição e Empresa como obrigatório.",
+            });
         }
+
+        return res.status(StatusCodes.CREATED).send({
+          Sucess: "Depoiemnto Atualizado, obrigado!",
+        });
+      }
+    } catch (error) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .send({ error: "Erro ao cadastrar depoimento." });
     }
+  }
 }
