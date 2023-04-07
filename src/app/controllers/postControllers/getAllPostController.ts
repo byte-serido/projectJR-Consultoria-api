561887d7b9472135ac9e7342fc09fbc6b83db07d
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-04-07 18:32:47 -0300

    :books: docs: doc updatePostController (#36)

--- src/app/controllers/postControllers/getAllPostController.ts
+++ src/app/controllers/postControllers/getAllPostController.ts
@@ -5,22 +5,36 @@ import { StatusCodes } from "http-status-codes";
 
 export const prisma = new PrismaClient();
 
+/**
+ *  Essa é a classe controller do GetAllPost
+ *
+ *  Antes de listar as postagens, ela testa se existe alguma postagem,
+ *  se não existir retorna status:
+ *  BAD_REQUEST = 400
+ *
+ *  Se correu tudo bem retorna o status:
+ *  OK = 200
+ *
+ *  Se ocorreu algum erro no processo retorna status:
+ *  BAD_REQUEST = 400
+ */
 export class GetAllPostController {
-    async handle(req: Request, res: Response) {
+  async handle(req: Request, res: Response) {
+    try {
+      const getAllPost = new GetAllPost();
+      const result = await getAllPost.execute();
 
-        try {
+      if (!result) {
+        return res
+          .status(StatusCodes.BAD_REQUEST)
+          .send({ error: "Nenhum post criado" });
+      }
 
-            const getAllPost = new GetAllPost();
-            const result = await getAllPost.execute()
-
-            if (!result) {
-                return res.status(StatusCodes.BAD_REQUEST).send({ error: "Nenhum post criado" })
-            }
-
-            return res.status(StatusCodes.OK).json(result);
-
-        } catch (err) {
-            return res.status(StatusCodes.BAD_REQUEST).send({error: "Erro na amostragem de post"})
-        }
+      return res.status(StatusCodes.OK).json(result);
+    } catch (err) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .send({ error: "Erro na amostragem de post" });
     }
+  }
 }
--- src/app/controllers/postControllers/updatePostController.ts
+++ src/app/controllers/postControllers/updatePostController.ts
@@ -5,38 +5,55 @@ import { StatusCodes } from "http-status-codes";
 
 export const prisma = new PrismaClient();
 
+/**
+ *  Essa é a classe controller do UpdatePost
+ *
+ *  Antes de editar uma postagem, ela testa se a postagem existe,
+ *  e se tem o titulo vazio, caso exista titulo vazio ou
+ *  não exita a postagem retorna status:
+ *  UNAUTHORIZED = 401
+ *
+ *  Se correu tudo bem, retorna o depoimento e status:
+ *  CREATED = 201
+ *
+ *  Se ocorreu algum erro no processo retorna status:
+ *  BAD_REQUEST = 400
+ */
 export class UpdatePost {
-    async handle(req: Request, res: Response) {
+  async handle(req: Request, res: Response) {
+    const { id, title, description, autor, imgUrl } = req.body;
 
-        const { id, title, description, autor, imgUrl, } = req.body;
+    try {
+      if (
+        await prisma.post.findMany({
+          where: {
+            id: id,
+          },
+        })
+      ) {
+        const UpdatePost = new UpadatePost();
+        await UpdatePost.execute({
+          id,
+          title,
+          description,
+          autor,
+          imgUrl,
+        });
 
-        try {
-
-            if (await prisma.post.findMany({
-                where: {
-                    id: id,
-                }
-
-            })) {
-
-                const UpdatePost = new UpadatePost();
-                await UpdatePost.execute({
-                    id, title, description, autor, imgUrl
-                });
-
-                if (!title) {
-                    return res.status(StatusCodes.UNAUTHORIZED).send({
-                        error: "O post deve ter titulo"
-                    });
-                }
-
-                return res.status(StatusCodes.CREATED).send({ sucess: "Post foi atualizado com sucesso!" })
-
-            }
-            
-        } catch (error) {
-            return res.status(StatusCodes.BAD_REQUEST).send({error: "Erro na atualização da postagem"}
-            );
+        if (!title) {
+          return res.status(StatusCodes.UNAUTHORIZED).send({
+            error: "O post deve ter titulo",
+          });
         }
-    };
+
+        return res
+          .status(StatusCodes.CREATED)
+          .send({ sucess: "Post foi atualizado com sucesso!" });
+      }
+    } catch (error) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .send({ error: "Erro na atualização da postagem" });
+    }
+  }
 }
