5fdf730370b69d69d5e9294c828c0df0cd7f8611
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-04-07 17:09:40 -0300

    :books: docs: doc gelAllDepoController (#36)

--- src/app/controllers/depositionsController/getAllDepoController.ts
+++ src/app/controllers/depositionsController/getAllDepoController.ts
@@ -5,24 +5,36 @@ import { StatusCodes } from "http-status-codes";
 
 export const prisma = new PrismaClient();
 
+/**
+ *  Essa é a classe controller do GetAllDepo
+ *
+ *  Antes de listar os depoimentos, ela testa se existe algum depoimento,
+ *  se não existir retorna status:
+ *  PAYMENT_REQUIRED = 402
+ *
+ *  Se correu tudo bem retorna o status:
+ *  OK = 200
+ *
+ *  Se ocorreu algum erro no processo retorna status 400.
+ *
+ */
 export class GelAllDepoController {
-    async handle(req: Request, res: Response) {
+  async handle(req: Request, res: Response) {
+    try {
+      const getAllDepo = new GetAllDepo();
+      const result = await getAllDepo.execute();
 
-        try {
+      if (!result) {
+        return res
+          .status(StatusCodes.PAYMENT_REQUIRED)
+          .send({ error: "Nenhum depoimento cadastrado" });
+      }
 
-            const getAllDepo = new GetAllDepo();
-            const result = await getAllDepo.execute()
-
-            if (!result) {
-                return res.status(StatusCodes.PAYMENT_REQUIRED).send({ error: "Nenhum depoimento cadastrado" })
-            }
-
-            return res.status(StatusCodes.OK).json(result);
-
-        } catch (error) {
-            return res.status(StatusCodes.NOT_FOUND).send({
-                error: "Erro na busca por depoimentos,"
-            })
-        }
+      return res.status(StatusCodes.OK).json(result);
+    } catch (error) {
+      return res.status(StatusCodes.NOT_FOUND).send({
+        error: "Erro na busca por depoimentos,",
+      });
     }
+  }
 }
