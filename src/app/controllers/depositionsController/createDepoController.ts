2312c117dd57a96adfbaa7e1794b0c626490c237
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-04-07 17:01:03 -0300

    :books: docs: doc createDepoController (#36)

--- src/app/controllers/depositionsController/createDepoController.ts
+++ src/app/controllers/depositionsController/createDepoController.ts
@@ -5,27 +5,46 @@ import { StatusCodes } from "http-status-codes";
 
 export const prisma = new PrismaClient();
 
+/**
+ *  Essa é a classe controller do CreateCDepo
+ *
+ *  Antes de criar um novo depoimento, ela testa se o depoimento existe e
+ *  se tem alguma informação vazia, caso exista informação vazia
+ *  retorna status:
+ *  UNAUTHORIZED = 401
+ *
+ *  Se correu tudo bem retorna o depoimento e status:
+ *  CREATED = 201
+ *
+ *  Se ocorreu algum erro no processo retorna status:
+ *  NOT_FOUND = 404
+ *
+ */
 export class CreateDepoController {
-    async handle(req: Request, res: Response) {
-        const {
-            name, testimony, office, company, imgUrl
-        } = req.body;
+  async handle(req: Request, res: Response) {
+    const { name, testimony, office, company, imgUrl } = req.body;
 
-        try {
-            
-            if (!name || !testimony || !company) {
-                return res.status(StatusCodes.UNAUTHORIZED).send({error: "Preencha as informações vazias"})
-            }
+    try {
+      if (!name || !testimony || !company) {
+        return res
+          .status(StatusCodes.UNAUTHORIZED)
+          .send({ error: "Preencha as informações vazias" });
+      }
 
-            const create = new CreateDepo();
-            const depo = await create.execute({ name, testimony, office, company, imgUrl });
+      const create = new CreateDepo();
+      const depo = await create.execute({
+        name,
+        testimony,
+        office,
+        company,
+        imgUrl,
+      });
 
-            return res.status(StatusCodes.CREATED).send({
-                depo
-            },);
-
-        } catch (error) {
-            return res.status(StatusCodes.NOT_FOUND).send(error)
-        }
+      return res.status(StatusCodes.CREATED).send({
+        depo,
+      });
+    } catch (error) {
+      return res.status(StatusCodes.NOT_FOUND).send(error);
     }
+  }
 }
