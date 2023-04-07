f8a4a1abfcd740659402c64115751e1a3136d566
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-04-07 18:03:00 -0300

    :books: docs: doc createMemberController (#36)

--- src/app/controllers/memberControllers/createMemberController.ts
+++ src/app/controllers/memberControllers/createMemberController.ts
@@ -3,24 +3,53 @@ import { PrismaClient } from "@prisma/client";
 import { CreateMember } from "../../usercases/memberUsercases/createMember";
 import { StatusCodes } from "http-status-codes";
 
-export const prisma = new PrismaClient;
+export const prisma = new PrismaClient();
 
-export class CreateMemberController{
-    async handle(req: Request, res: Response){
-        const {name,registration,number,role,description,imgUrl} = req.body;
-        try{
-            if(await prisma.member.findUnique({where:{registration:registration}})){
-                return res.status(StatusCodes.UNAUTHORIZED).send({error:"Membro já existente"})
-            }
+/**
+ *  Essa é a classe controller do CreateCMember
+ *
+ *  Antes de criar um novo membro, ela testa se o membro existe e
+ *  se tem alguma informação vazia, caso exista informação vazia
+ *  retorna status:
+ *  UNAUTHORIZED = 401
+ *
+ *  Se correu tudo bem retorna o membro.
+ *
+ *  Se ocorreu algum erro no processo retorna status:
+ *  NOT_FOUND = 404
+ *
+ */
+export class CreateMemberController {
+  async handle(req: Request, res: Response) {
+    const { name, registration, number, role, description, imgUrl } = req.body;
+    try {
+      if (
+        await prisma.member.findUnique({
+          where: { registration: registration },
+        })
+      ) {
+        return res
+          .status(StatusCodes.UNAUTHORIZED)
+          .send({ error: "Membro já existente" });
+      }
 
-            const create = new CreateMember();
-            const member = await create.execute({name,registration,number,role,description,imgUrl});
-        
-            return res.send({
-                member,
-            },);
-        } catch(err){
-            return res.status(StatusCodes.NOT_FOUND).send({error:"Falha no Registro de membro"});
-        }
-    };
+      const create = new CreateMember();
+      const member = await create.execute({
+        name,
+        registration,
+        number,
+        role,
+        description,
+        imgUrl,
+      });
+
+      return res.send({
+        member,
+      });
+    } catch (err) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .send({ error: "Falha no Registro de membro" });
+    }
+  }
 }
