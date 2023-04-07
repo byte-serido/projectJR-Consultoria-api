547a73cef33043ef5829b7ff6de94b9d3857abd4
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-04-07 19:07:44 -0300

    :books: docs: doc getAllUsersController (#36)

--- src/app/controllers/userControllers/getAllUsersController.ts
+++ src/app/controllers/userControllers/getAllUsersController.ts
@@ -1,20 +1,30 @@
 import { Request, Response } from "express";
 import { GetUsers } from "../../usercases/userUserCases/getAllUser";
 
+/**
+ *  Essa é a classe controller do GetAllUsers
+ *
+ *  Antes de listar todos os usuários, ela testa se existe algum ususário,
+ *  se não existir retorna status 400.
+ *
+ *  Se correu tudo bem retorna o status 201.
+ *
+ *  Se ocorreu algum erro no processo retorna status 400.
+ *
+ */
+export class GetUserController {
+  async handle(req: Request, res: Response) {
+    try {
+      const getUsers = new GetUsers();
+      const result = await getUsers.execute();
 
-export class GetUserController{
-    async handle(req: Request, res: Response){
-        try{
-            const getUsers = new GetUsers;
-            const result = await getUsers.execute();
-            
-            if(!Object.keys(result).length){
-                return res.status(400).json({error:"Não há usuários cadastrados"});
-            }
-            
-            return res.status(201).json(result);
-        } catch(err){
-            return res.status(400).send({error:"Não há usuários cadastrados"});
-        }
+      if (!Object.keys(result).length) {
+        return res.status(400).json({ error: "Não há usuários cadastrados" });
+      }
+
+      return res.status(201).json(result);
+    } catch (err) {
+      return res.status(400).send({ error: "Não há usuários cadastrados" });
     }
+  }
 }
