8f490851d39445222c5b0474e0ee73db2d929e1a
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-04-07 15:24:19 -0300

    :books: docs: doc getAllCommentsController (#36)

--- src/app/controllers/commentControllers/getAllCommentsController.ts
+++ src/app/controllers/commentControllers/getAllCommentsController.ts
@@ -4,6 +4,16 @@ import { GetAllComments } from "../../usercases/commentUserCases/getAllComments"
 
 export const prisma = new PrismaClient();
 
+/**
+ *  Essa é a classe controller do GetAllComment
+ *
+ *  Antes de listar todos os comentários, ela testa se os comentários existem.
+ *
+ *  Se correu tudo bem retorna o comentario e o status 200.
+ *
+ *  Se ocorreu algum erro no processo retorna status 400.
+ *
+ */
 export class GetAllCommentsController {
   async handle(req: Request, res: Response) {
     try {
