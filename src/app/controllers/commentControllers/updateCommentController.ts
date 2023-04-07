9e9e17e845a7ef8cd97dc146e42857e041e94a2d
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-04-07 15:26:52 -0300

    :books: docs: doc updateCommentController (#36)

--- src/app/controllers/commentControllers/updateCommentController.ts
+++ src/app/controllers/commentControllers/updateCommentController.ts
@@ -4,6 +4,17 @@ import { UpdateComment } from "../../usercases/commentUserCases/updateComment";
 
 export const prisma = new PrismaClient();
 
+/**
+ *  Essa é a classe controller do UpdateComment
+ *
+ *  Antes de editar um comentário, ela testa se o comentário existe,
+ *  se o comentário está com o autor ou se o texto está vazio.
+ *
+ *  Se correu tudo bem retorna o comentario e o status 200.
+ *
+ *  Se ocorreu algum erro no processo retorna status 400.
+ *
+ */
 export class UpdateCommentController {
   async handle(req: Request, res: Response) {
     const { id, authorName, text, posts } = req.body;
@@ -34,9 +45,14 @@ export class UpdateCommentController {
 
       return res
         .status(200)
-        .send({ comment, sucess: "Atualização de comentário realizada com sucesso!" });
+        .send({
+          comment,
+          sucess: "Atualização de comentário realizada com sucesso!",
+        });
     } catch (error) {
-      return res.status(400).send({ error: "A atualização do comentário falhou!" });
+      return res
+        .status(400)
+        .send({ error: "A atualização do comentário falhou!" });
     }
   }
 }
