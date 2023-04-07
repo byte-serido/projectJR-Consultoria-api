c9c2b295afc3eecfe30d7ed5584a3d956c19ed03
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-04-07 18:14:07 -0300

    :books: docs: doc gelMemberController (#36)

--- src/app/controllers/memberControllers/getMemberController.ts
+++ src/app/controllers/memberControllers/getMemberController.ts
@@ -2,19 +2,34 @@ import { Request, Response } from "express";
 import { GetMembers } from "../../usercases/memberUsercases/getMembers";
 import { StatusCodes } from "http-status-codes";
 
-export class GetMembersController{
-    async handle(req: Request, res: Response){
-        try{
-            const getMembers = new GetMembers();
-            const result = await getMembers.execute();
-            
-            if(!Object.keys(result).length){
-                return res.status(400).json({error:"Não há membros cadastrados"});
-            }
-            
-            return res.status(StatusCodes.OK).json(result);
-        } catch(err){
-            return res.status(StatusCodes.BAD_REQUEST).send({error:"Não há membros cadastrados"});
-        }
+/**
+ *  Essa é a classe controller do getMember
+ *
+ *  Ela busca um mebro, antes disso ela testa se o membro existe,
+ *  se não existir retorna status 400.
+ *
+ *  Se correu tudo bem retorna o membro e status:
+ *  OK = 200
+ *
+ *  Se ocorreu algum erro no processo retorna status:
+ *  BAD_REQUEST = 400
+ *
+ */
+export class GetMembersController {
+  async handle(req: Request, res: Response) {
+    try {
+      const getMembers = new GetMembers();
+      const result = await getMembers.execute();
+
+      if (!Object.keys(result).length) {
+        return res.status(400).json({ error: "Não há membros cadastrados" });
+      }
+
+      return res.status(StatusCodes.OK).json(result);
+    } catch (err) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .send({ error: "Não há membros cadastrados" });
     }
+  }
 }
