bbe7b6a56ef7413ae45cf8cc362c897c9ee2d1ae
JFmaia <jfmaia741@gmail.com>
2023-05-23 21:33:08 -0300

    feat: Adicionando nova rota

--- src/routes/member.routes.ts
+++ src/routes/member.routes.ts
@@ -3,15 +3,18 @@ import { CreateMemberController } from "../app/controllers/memberControllers/cre
 import { GetMembersController } from "../app/controllers/memberControllers/getMemberController";
 import { UpdateMemberController } from "../app/controllers/memberControllers/updateMemberController";
 import { DeletMemberController } from "../app/controllers/memberControllers/deleteMemberController";
+import {GetOneMemberController} from "../app/controllers/memberControllers/getOneMemberController";
 
 const createMemberController = new CreateMemberController();
 const getMembersController = new GetMembersController();
 const updateMemberController = new UpdateMemberController();
 const deletMemberController = new DeletMemberController();
+const getOneMemberController = new GetOneMemberController();
 const memberRoutes = Router();
 
 memberRoutes.post("/create",createMemberController.handle);
 memberRoutes.get("/getall",getMembersController.handle);
+memberRoutes.get("/getone",getOneMemberController.handle);
 memberRoutes.put("/update",updateMemberController.handle);
 memberRoutes.delete("/delete",deletMemberController.handle);
 
