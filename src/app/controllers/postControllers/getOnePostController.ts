db45b2881540dceebdb67189064be0c5c06a4991
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-06-25 17:33:54 -0300

    ✨ feat: add getOnePostController.ts (#46)

---
+++ src/app/controllers/postControllers/getOnePostController.ts
@@ -0,0 +1,47 @@
+import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+
+export const prisma = new PrismaClient();
+
+/**
+ *  Essa é a classe controller do getOnePostController
+ *
+ *  Ela retorna um post e os comentarios buscando pelo id
+ * 
+ *  Antes ela testa se o post existe,
+ *  se não existir retorna status:
+ *  NOT_FOUND = 404
+ 
+ *
+ *  Se correu tudo bem retorna status:
+ *  OK = 200
+ *
+ *  Se ocorreu algum erro no processo retorna status:
+ *  BAD_REQUEST = 400
+ *
+ */
+export class GetOnePostController {
+  async handle(req: Request, res: Response) {
+    try {
+      const { id } = req.params;
+      const post = await prisma.post.findUnique({
+        where: {
+          id: id,
+        },
+      });
+
+      if (!post) {
+        return res
+          .status(StatusCodes.NOT_FOUND)
+          .send({ error: "Membro não existe!" });
+      }
+
+      return res.status(StatusCodes.OK).json(post);
+    } catch (err) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .send({ error: "Erro ao buscar o membro!" });
+    }
+  }
+}
