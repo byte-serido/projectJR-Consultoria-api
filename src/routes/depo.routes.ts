f57e600efa32f953eb39a15f25fc63dc2a093998
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-06-29 23:44:38 -0300

    ✨ feat: mod depo.routes (#46)

--- src/routes/depo.routes.ts
+++ src/routes/depo.routes.ts
@@ -3,18 +3,20 @@ import { CreateDepoController } from "../app/controllers/depositionsController/c
 import { GelAllDepoController } from "../app/controllers/depositionsController/getAllDepoController";
 import { UpdateDepos } from "../app/controllers/depositionsController/updateDepoController";
 import { DeleteDepoController } from "../app/controllers/depositionsController/deleteDepoController";
+import { GetOneDepoController } from "../app/controllers/depositionsController/getOneDepoController";
 
 const depoRoutes = Router();
 
 const createDepo = new CreateDepoController();
-const getAllDepo = new GelAllDepoController()
+const getAllDepo = new GelAllDepoController();
 const upDepo = new UpdateDepos();
 const deleteDepo = new DeleteDepoController();
+const getOneDepo = new GetOneDepoController();
 
 depoRoutes.post("/create", createDepo.handle);
 depoRoutes.get("/getall", getAllDepo.handle);
 depoRoutes.put("/update", upDepo.handle);
 depoRoutes.delete("/delete", deleteDepo.handle);
+depoRoutes.get("/getone:id", getOneDepo.handle);
 
-
-export {depoRoutes}
+export { depoRoutes };
