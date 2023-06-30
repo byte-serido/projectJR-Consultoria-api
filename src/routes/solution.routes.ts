ba15deaed876ed7995279ee43e5a8ae6e7d28b8a
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-06-29 23:58:40 -0300

    ✨ feat: mod solution.routes (#46)

--- src/routes/solution.routes.ts
+++ src/routes/solution.routes.ts
@@ -3,16 +3,19 @@ import { CreateSolutionController } from "../app/controllers/solutionsController
 import { GetAllSolutionController } from "../app/controllers/solutionsControllers/getSolutionsController";
 import { UpdateSolutionController } from "../app/controllers/solutionsControllers/updateSolutionController";
 import { DeleteSolutionController } from "../app/controllers/solutionsControllers/deleteSolutionController";
+import { GetOneSolutionController } from "../app/controllers/solutionsControllers/getOneSolutionController";
 
 const createSolutionController = new CreateSolutionController();
 const getAllSolutionController = new GetAllSolutionController();
 const updateSolutionController = new UpdateSolutionController();
 const deleteSolutionController = new DeleteSolutionController();
+const getOneSolutionController = new GetOneSolutionController();
 const solutionRoutes = Router();
 
 solutionRoutes.post("/create", createSolutionController.handle);
 solutionRoutes.get("/getall", getAllSolutionController.handle);
 solutionRoutes.put("/update", updateSolutionController.handle);
 solutionRoutes.delete("/delete", deleteSolutionController.handle);
+solutionRoutes.get("/getone:id", getOneSolutionController.handle);
 
 export { solutionRoutes };
