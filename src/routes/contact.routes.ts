aeca4d0d98a9af26d368e2cbc19ffd4faa218d97
FlaviodosSantos <flaviovorthrox@yahoo.com.br>
2023-06-29 23:29:10 -0300

    ✨ feat: mod contact.routes (#46)

--- src/routes/contact.routes.ts
+++ src/routes/contact.routes.ts
@@ -3,16 +3,19 @@ import { CreateContactController } from "../app/controllers/contactControllers/c
 import { DeleteContactController } from "../app/controllers/contactControllers/deleteContactController";
 import { GetAllContactsController } from "../app/controllers/contactControllers/getAllContactsController";
 import { UpdateContactController } from "../app/controllers/contactControllers/updateContactController";
+import { GetOneContactController } from "../app/controllers/contactControllers/getOneContactController";
 
 const createContactController = new CreateContactController();
 const getAllContactsController = new GetAllContactsController();
 const updateContactController = new UpdateContactController();
 const deleteContactController = new DeleteContactController();
+const getOneContactController = new GetOneContactController();
 const contactRoutes = Router();
 
 contactRoutes.post("/create", createContactController.handle);
 contactRoutes.get("/getall", getAllContactsController.handle);
 contactRoutes.put("/update", updateContactController.handle);
 contactRoutes.delete("/delete", deleteContactController.handle);
+contactRoutes.get("/getone:id", getOneContactController.handle);
 
 export { contactRoutes };
